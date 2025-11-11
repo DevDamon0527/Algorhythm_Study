0df27829a5262238ad870f9abd6698d772087d52
DevDamon0527 <devdamon0527@gmail.com>
2025-11-11 15:31:27 +0900

    BaekJoon Code 251111_10430

---
+++ BaekJoon/solutions/Bronze/10430.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let [A, B, C] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+console.log((A+B)%C);
+console.log(((A%C) + (B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C) * (B%C))%C);

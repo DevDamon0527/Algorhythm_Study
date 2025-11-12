d6d6f592a10609985db11e8f541285ded2eb1678
DevDamon0527 <devdamon0527@gmail.com>
2025-11-12 18:40:14 +0900

    BaekJoon Code 251112

--- BaekJoon/solutions/Bronze/10809.js
+++ BaekJoon/solutions/Bronze/10809.js
@@ -1,24 +1,18 @@
+const { log } = require('console');
 let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().trim().split('');
-
-console.log(input);
-
-// 소문자 단어 S
-// 각각 알파벳, 단어에 포함되어 있는 경우에는 처음 등장하는 위치(index)
-// 포함되어 있지 않은 경우 -1 출력
-
-// let str = '';
-// for (let i = 1; i <= 26; i++) {
-//     a
-// }
-
-// ['b', 'a', 'e','k', 'j', 'o', 'o', 'n']
-let x = input.indexOf('a');
-console.log(x);
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('');
 
 let arr = [];
 
 for (let i = 97; i <= 122; i++) {
     arr.push(String.fromCharCode(i));
 }
-console.log(arr);
+
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+    let alphabet = arr[i];
+    let index = input.indexOf(alphabet);
+    result.push(index);
+}
+console.log(result.join(' '));

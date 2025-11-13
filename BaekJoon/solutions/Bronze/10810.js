d9bfa95590efa9cd18d95a990a4465714d1db2a2
DevDamon0527 <devdamon0527@gmail.com>
2025-11-13 23:59:04 +0900

    commit test

---
+++ BaekJoon/solutions/Bronze/10810.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().trim();
+
+
+// 바구니 N개 / 1~N 번호 / 바구니 공 x 공 1개씩
+// M 번 넣을 예정
+// 1번 넣을 때 넣을 바구니 범위를 정하고, 정한 바구니에 모두 같은 번호가 적혀있는 공을 넣는다.
+

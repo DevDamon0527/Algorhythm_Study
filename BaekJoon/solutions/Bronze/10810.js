f6e6bbae4ad2855ef453d510443278ba4ff811ef
DevDamon0527 <devdamon0527@gmail.com>
2025-11-20 19:48:32 +0900

    BaekJoon Code 251120

--- BaekJoon/solutions/Bronze/10810.js
+++ BaekJoon/solutions/Bronze/10810.js
@@ -1,8 +1,14 @@
 let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().trim();
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
+const [N, M] = input[0].split(" ").map(Number);
+const basket = Array(N).fill(0);
 
-// 바구니 N개 / 1~N 번호 / 바구니 공 x 공 1개씩
-// M 번 넣을 예정
-// 1번 넣을 때 넣을 바구니 범위를 정하고, 정한 바구니에 모두 같은 번호가 적혀있는 공을 넣는다.
+for (let idx = 1; idx <= M; idx++) {
+    const [i, j, k] = input[idx].split(" ").map(Number);
+    for (let x = i - 1; x < j; x++) {
+        basket[x] = k;
+    }
+}
 
+console.log(basket.join(" "));

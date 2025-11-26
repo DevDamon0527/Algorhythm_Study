9674b0e2de1d27f6115c2664b5e8d017dead0f95
DevDamon0527 <devdamon0527@gmail.com>
2025-11-26 23:54:40 +0900

    BaekJoon Code 251126

---
+++ BaekJoon/solutions/Bronze/10818.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+
+const numbers = input[1].split(' ').map(Number);
+
+const min = Math.min(...numbers);
+const max = Math.max(...numbers);
+
+console.log(min, max);

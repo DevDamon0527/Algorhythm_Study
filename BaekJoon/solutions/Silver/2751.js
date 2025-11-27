dc69d2ab135fa49b1520c03b05bb4556339e1f89
DevDamon0527 <devdamon0527@gmail.com>
2025-11-27 18:53:36 +0900

    BaekJoon Code 251127

---
+++ BaekJoon/solutions/Silver/2751.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+const arr = input.slice(1).map(Number);
+
+arr.sort((a, b) => a - b);
+
+console.log(arr.join('\n'));
